dee01d2b4383a7b6f983e7949d90c9091be50b10
shiv kumar saini <141953893+shiv852@users.noreply.github.com>
2025-11-01 17:46:09 +0530

    Update updatefrontendnew.sh

--- Automations/updatefrontendnew.sh
+++ Automations/updatefrontendnew.sh
@@ -13,9 +13,9 @@ file_to_find="../frontend/.env.docker"
 current_url=$(cat $file_to_find)
 
 # Update the .env file if the IP address has changed
-if [[ "$current_url" != "VITE_API_PATH=\"http://${ipv4_address}:31100\"" ]]; then
+if [[ "$current_url" != "VITE_API_PATH=\"http://${ipv4_address}:3000\"" ]]; then
     if [ -f $file_to_find ]; then
-        sed -i -e "s|VITE_API_PATH.*|VITE_API_PATH=\"http://${ipv4_address}:31100\"|g" $file_to_find
+        sed -i -e "s|VITE_API_PATH.*|VITE_API_PATH=\"http://${ipv4_address}:3000\"|g" $file_to_find
     else
         echo "ERROR: File not found."
     fi
