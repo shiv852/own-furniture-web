966046790f8d405d70ff38ac060f06470aeced28
shiv kumar saini <141953893+shiv852@users.noreply.github.com>
2025-11-04 23:18:11 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose")
 
 async function connecteDb(){
   try{
-    mongoose.connect(`${process.env.MONGODB_URL}/furnitureproject`)
+    mongoose.connect(`${process.env.MONGODB_URL}/furniture`)
     console.log("connected to the database")
   }catch(err){
     console.log(err)
