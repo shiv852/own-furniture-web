4795b2da3785330296dd430f828fd0fe927dc08b
shiv kumar saini <141953893+shiv852@users.noreply.github.com>
2025-11-08 13:44:04 +0530

    Update api.js

--- finalProject/src/common/api.js
+++ finalProject/src/common/api.js
@@ -3,8 +3,8 @@ import axios from 'axios';
 
 const api = axios.create({
   // baseURL:'http://jaipurbestfurniture.shop/api'    //if i will run this application on domain 
- // baseURL:'http://localhost:3000/api'         //if my application running on localhost 
-    baseURL:'http://13.203.220.54:31000/api'    //if my application running on this port
+      baseURL:'http://localhost:3000/auth'        //if my application running on localhost 
+  // baseURL:'http://13.203.220.54:31000/api'    //if my application running on this port
    
 });
 export const googleAuth =(code) => api.get(`/google?code=${code}`)
